6cc7b7ebf56faf97d88b674c0fd62c7c968e639f
shangjie-li <1814481684@qq.com>
2021-03-23 01:49:45 +0800

    Add launch files

---
+++ launch/sensors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /opt/ros/kinetic/setup.bash
+source /home/seucat/shangjie/detection-and-tracking/ros_ws/devel/setup.bash
+
+roslaunch /home/seucat/shangjie/detection-and-tracking/ros_ws/src/detection_and_tracking/launch/sensors.launch
+
+
