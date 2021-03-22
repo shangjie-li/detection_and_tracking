8333bb693a7019c696c8879e565d54450f39f90b
shangjie-li <1814481684@qq.com>
2021-03-22 23:55:26 +0800

    delete launch files

--- launch/sensors.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/kinetic/setup.bash
-source /home/seucat/shangjie/detection-and-tracking/ros_ws/devel/setup.bash
-
-roslaunch /home/seucat/shangjie/detection-and-tracking/ros_ws/src/detection_and_tracking/launch/sensors.launch
-
-
