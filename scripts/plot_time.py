268807f099f216cab67ed0588e7c980992f28c50
shangjie-li <1814481684@qq.com>
2021-04-19 19:14:48 +0800

    Add plot of Time cost - Number of objects

--- scripts/plot_time.py
+++ scripts/plot_time.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == '__main__':
     # 读入结果文件
@@ -19,6 +20,16 @@ if __name__ == '__main__':
     time_tracking_list = []
     time_display_list = []
     time_all_list = []
+    
+    # 统计不同目标数量(0 ~ num - 1)对应的平均耗时，frequencys等列表的索引为目标数量
+    num = 20
+    frequencys = [0] * num
+    costs_segmentation = [0] * num
+    costs_projection = [0] * num
+    costs_fusion = [0] * num
+    costs_tracking = [0] * num
+    costs_display = [0] * num
+    costs_all = [0] * num
     
     # 逐行读取
     num_frame = len(infos)
@@ -96,32 +107,93 @@ if __name__ == '__main__':
         time_all = float(temp_str)
         time_all_list.append(time_all)
         
+        # 统计不同目标数量对应的计算用时
+        frequencys[amount] += 1
+        costs_segmentation[amount] += time_segmentation
+        costs_projection[amount] += time_projection
+        costs_fusion[amount] += time_fusion
+        costs_tracking[amount] += time_tracking
+        costs_display[amount] += time_display
+        costs_all[amount] += time_all
+        
         print('frame:%d amount:%d segmentation:%f projection:%f fusion:%f tracking:%f display:%f all:%f' % (
             frame, amount, time_segmentation, time_projection, time_fusion, time_tracking, time_display, time_all))
     
+    # 计算平均值
+    for j in range(len(frequencys)):
+        if frequencys[j] != 0:
+            costs_segmentation[j] /= frequencys[j]
+            costs_projection[j] /= frequencys[j]
+            costs_fusion[j] /= frequencys[j]
+            costs_tracking[j] /= frequencys[j]
+            costs_display[j] /= frequencys[j]
+            costs_all[j] /= frequencys[j]
+    
+    amounts = []
+    costs_segmentation_a = []
+    costs_projection_a = []
+    costs_fusion_a = []
+    costs_tracking_a = []
+    costs_display_a = []
+    costs_all_a = []
+    for j in range(len(frequencys)):
+        if frequencys[j] != 0 and j >= 4:
+            amounts.append(j)
+            costs_segmentation_a.append(costs_segmentation[j])
+            costs_projection_a.append(costs_projection[j])
+            costs_fusion_a.append(costs_fusion[j])
+            costs_tracking_a.append(costs_tracking[j])
+            costs_display_a.append(costs_display[j])
+            costs_all_a.append(costs_all[j])
+    
     # 设置画布
-    fig, axes = plt.subplots(2, 1, figsize=(5, 6))
+    fig, axes = plt.subplots(3, 1, figsize=(5, 8))
     fig.canvas.set_window_title('Time cost')
     plt.subplots_adjust(left=0.15, right=0.85, bottom=0.1, top=0.9, wspace=0.4, hspace=0.4)
     
     # 绘制amount
-    axes[0].plot(frame_list, amount_list, color='red', linestyle='-', linewidth=1, label='amount      ')
-    axes[0].set_ylim(-0.5, 13)
-    axes[0].legend(loc='upper right', fontsize=8, ncol=1)
+    axes[0].plot(frame_list, amount_list, color='deeppink', linestyle='-', linewidth=1.3, label='amount')
+    axes[0].tick_params(direction='in')
+    axes[0].set_xticks([0, 50, 100, 150, 200, 250])
+    axes[0].set_yticks([2, 4, 6, 8, 10, 12])
+    axes[0].set_xlim(0, 280)
+    axes[0].set_ylim(2, 12)
+    axes[0].spines['right'].set_color('none')
+    axes[0].spines['top'].set_color('none')
     axes[0].set_title('Number of objects - Frame', fontsize=10)
     
     # 绘制time_cost
-    axes[1].plot(frame_list, time_segmentation_list, color='green', linestyle='-', linewidth=1, label='segmentation')
-    axes[1].plot(frame_list, time_projection_list, color='purple', linestyle='-', linewidth=1, label='projection  ')
-    axes[1].plot(frame_list, time_fusion_list, color='deepskyblue', linestyle='-', linewidth=1, label='fusion      ')
-    axes[1].plot(frame_list, time_tracking_list, color='orange', linestyle='-', linewidth=1, label='tracking    ')
-    axes[1].plot(frame_list, time_all_list, color='red', linestyle='-', linewidth=1, label='all         ')
+    axes[1].plot(frame_list, time_segmentation_list, color='darkorange', linestyle='-', linewidth=1.3, label='segment')
+    axes[1].plot(frame_list, time_projection_list, color='slategray', linestyle='-', linewidth=1.3, label='project')
+    axes[1].plot(frame_list, time_fusion_list, color='cornflowerblue', linestyle='-', linewidth=1.3, label='fuse   ')
+    axes[1].plot(frame_list, time_tracking_list, color='mediumseagreen', linestyle='-', linewidth=1.3, label='track  ')
+    axes[1].plot(frame_list, time_all_list, color='deeppink', linestyle='-', linewidth=1.3, label='all    ')
+    axes[1].tick_params(direction='in')
+    axes[1].set_xticks([0, 50, 100, 150, 200, 250])
     axes[1].set_yticks([0, 0.05, 0.10])
-    axes[1].set_ylim(-0.005, 0.13)
-    axes[1].legend(loc='upper center', fontsize=8, ncol=3)
+    axes[1].set_xlim(0, 280)
+    axes[1].set_ylim(-0.005, 0.10)
+    axes[1].spines['right'].set_color('none')
+    axes[1].spines['top'].set_color('none')
+    axes[1].legend(loc='upper right', fontsize=8, ncol=3)
     axes[1].set_title('Time cost - Frame', fontsize=10)
     
+    # 绘制目标数量与耗时之间的关系
+    axes[2].bar(amounts, costs_all_a, width=0.35, color='deeppink', label='all')
+    axes[2].tick_params(direction='in')
+    axes[2].set_xticks([4, 5, 6, 7, 8, 9, 10])
+    axes[2].set_yticks([0.05, 0.06, 0.07, 0.08])
+    axes[2].set_xlim(3.5, 10.5)
+    axes[2].set_ylim(0.05, 0.08)
+    axes[2].spines['right'].set_color('none')
+    axes[2].spines['top'].set_color('none')
+    axes[2].tick_params(bottom=False, top=False, left=True, right=False)
+    for x, y in zip(amounts, costs_all_a):
+        axes[2].text(x - 0.285, y + 0.001, '%.3f' % y, fontsize=8)
+    axes[2].set_title('Time cost - Number of objects', fontsize=10)
+    
     fig.savefig('amount and time cost.png', dpi=100)
+    
     plt.show()
     
     
